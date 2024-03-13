d4a8461af11e53298e550a65286dd08bca090377
Ashish Shubham <ashish.shubham@thoughtspot.com>
2024-03-13 11:34:12 +0530

    cors

---
+++ api/check.ts
@@ -0,0 +1,14 @@
+export const config = {
+    runtime: "experimental-edge",
+};
+
+export default async (req) => {
+    return new Response(JSON.stringify({
+        message: "success"
+    }), {
+        status: 200,
+        headers: {
+            'content-type': 'application/json;charset=UTF-8',
+        }
+    });
+}
