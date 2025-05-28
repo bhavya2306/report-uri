77388f9d854e2de1af0f8e83ecabf4cc2e383be3
Bhavya Jain <bhavya.jain@thoughtspot.com>
2025-05-29 01:15:46 +0530

    Prevent unhandled rejections

--- api/report.ts
+++ api/report.ts
@@ -4,15 +4,13 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 function parseCSPData(headers, body) {
-    var userAgent = headers['user-agent'];
-    var referer = headers['referer'];
-    var xffor = headers['x-forwarded-for'];
-    var timestamp = new Date().toISOString();
-    var cspData;
+    const userAgent = headers['user-agent'];
+    const referer = headers['referer'];
+    const xffor = headers['x-forwarded-for'];
+    let cspData = {};
 
 
     try {
-        cspData = {};
         cspData["client-ip"] = xffor;
         cspData["user-agent"] = userAgent;
         cspData["immediate-referer"] = referer;
@@ -28,6 +26,7 @@ function parseCSPData(headers, body) {
         }
     }
     catch (e) {
+        console.error("Error in parseCSPData", e);
         cspData["parse"] = "error";
         cspData["error"] = e;
     }
@@ -37,19 +36,18 @@ function parseCSPData(headers, body) {
 
 const clusterDetailsCache = new Map();
 async function getClusterDetails(documentUri: string): Promise<any> {
-    const url = new URL(documentUri);
-    if (clusterDetailsCache.has(url.origin)) {
-        return clusterDetailsCache.get(documentUri);
-    }
-
     try {
+        const url = new URL(documentUri);
+        if (clusterDetailsCache.has(url.origin)) {
+            return clusterDetailsCache.get(documentUri);
+        }
         console.log(url.origin);
         const response = await fetch(`${url.origin}/prism/preauth/info`, { signal: AbortSignal.timeout(20000) });
         const { config } = await response.json();
         clusterDetailsCache.set(url.origin, config.mixpanelConfig);
         return config.mixpanelConfig;
     } catch (e) {
-        console.error(e);
+        console.error("Error in getClusterDetails", e);
         return null;
     }
 }
@@ -59,7 +57,7 @@ function getMixpanelKey(clusterDetails, documentUri) {
         return (clusterDetails.production) ? clusterDetails.prodSdkKey : clusterDetails.devSdkKey
     }
 
-    if (documentUri.includes(':')) {
+    if (documentUri && documentUri.includes(':')) {
         return process.env.MIXPANEL_DEV_KEY;
     } else {
         return process.env.MIXPANEL_PROD_KEY;
@@ -69,11 +67,10 @@ function getMixpanelKey(clusterDetails, documentUri) {
 async function trackCSPReport(cspData) {
     const documentUri = cspData['document-uri'];
     const clusterDetails = await getClusterDetails(documentUri);
-
     const key = getMixpanelKey(clusterDetails, documentUri);
-    const mixpanel = Mixpanel.init(key);
     return new Promise<void>((resolve, reject) => {
         try {
+            const mixpanel = Mixpanel.init(key);
             mixpanel.track('csp-report', {
                 ...cspData,
                 clusterId: clusterDetails?.clusterId,
@@ -81,14 +78,16 @@ async function trackCSPReport(cspData) {
                 hostAppUrl: cspData.referrer,
             }, (e) => {
                 if (e) {
-                    console.error(e);
+                    console.error("Error in Mixpane track", e);
                     reject(e);
                 }
-                resolve();
-                console.log('csp-report tracked');
-            });
+                else {
+                    console.log('csp-report tracked');
+                    resolve();
+                }
+            })
         } catch (e) {
-            console.trace(e);
+            console.error("Error in trackCSPReport", e);
             reject(e);
         }
     });
@@ -96,17 +95,27 @@ async function trackCSPReport(cspData) {
 
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-    const chunks = [];
-    req.on('data', chunk => {
-        chunks.push(chunk);
-    })
+    try {
+        const chunks = [];
+        req.on('data', chunk => {
+            chunks.push(chunk);
+        });
 
-    req.on('end', async () => {
-        const data = Buffer.concat(chunks);
-        const body = JSON.parse(data.toString());
-        console.log("CSP Report received", body);
-        var cspData = parseCSPData(req.headers, body);
-        await trackCSPReport(cspData);
-        return res.json(cspData);
-    });
+        req.on('end', async () => {
+            try {
+                const data = Buffer.concat(chunks);
+                const body = JSON.parse(data.toString());
+                console.log("CSP Report received", body);
+                const cspData = parseCSPData(req.headers, body);
+                await trackCSPReport(cspData);
+                return res.status(204).end();
+            } catch (e) {
+                console.error("Error in req.on('end')", e);
+                return res.end();
+            }
+        });
+    } catch (e) {
+        console.trace(e);
+        return res.end();
+    }
 }
