cacb89fcc2c8b7873da24b48e3b94f3b93f8348c
Bhavya Jain <bhavya.jain@thoughtspot.com>
2025-06-25 18:51:18 +0530

    Added Access-Control-Allow-Private-Network: true

--- api/report.ts
+++ api/report.ts
@@ -110,6 +110,7 @@ export default async (req: VercelRequest, res: VercelResponse) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.setHeader('Access-Control-Allow-Private-Network', 'true');
         
         res.status(204).end();
         return;
